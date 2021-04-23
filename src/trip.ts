18040353dd6135ced5d169f2884bddd7b46745fe
Jake <jacob.michael.mayer@gmail.com>
2021-04-23 09:31:02 -0400

    driver is an attribute on the trip

--- src/trip.ts
+++ src/trip.ts
@@ -1,7 +1,13 @@
+import { Driver } from './driver';
+
 export class Trip {
-    constructor(private mileageValue: number) {}
+    constructor(private driveValue: Driver, private mileageValue: number) {}
 
     get mileage(): number {
         return this.mileageValue;
+    }
+
+    get driver(): string {
+        return this.driveValue.name;
     }
 }
--- test/trip.test.ts
+++ test/trip.test.ts
@@ -1,10 +1,18 @@
 import { strictEqual } from 'assert';
+import { Driver } from '../src/driver';
 import { Trip } from '../src/trip';
 
 describe(Trip.name, () => {
+    const mileage = 12.3;
+    const driver = 'frank';
+    let trip: Trip;
+    before(() => {
+        trip = new Trip(new Driver(driver), mileage);
+    });
     it('#mileage', () => {
-        const mileage = 12.3;
-        const trip: Trip = new Trip(mileage);
         strictEqual(trip.mileage, mileage);
+    });
+    it('#driver', () => {
+        strictEqual(trip.driver, driver);
     });
 });
