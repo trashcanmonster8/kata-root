73915d63463b21a92c74f0201e5f612704810859
Jake <jacob.michael.mayer@gmail.com>
2021-04-22 23:59:14 -0400

    initial driver class

---
+++ src/driver.ts
@@ -0,0 +1,3 @@
+export class Driver {
+    constructor(public name: String) {}
+}
---
+++ test/driver.test.ts
@@ -0,0 +1,9 @@
+import { Driver } from "../src/driver"
+import {strictEqual } from 'assert'
+
+describe(Driver.name, () => {
+    it("has name", () => {
+        const driver = new Driver("test");
+        strictEqual(driver.name, "test");
+    });
+});
