42410af564e04556ea90e48d9f74ca2847ea6cda
Jake <jacob.michael.mayer@gmail.com>
2021-04-23 23:03:29 -0400

    added single driver case

--- src/summary.ts
+++ src/summary.ts
@@ -1,7 +1,22 @@
+import { Driver } from './driver';
 import { Output } from './output';
 
 export class Summary {
+    private drivers: Driver[] = [];
+
     public calculate(): Output[] {
-        return [];
+        const result: Output[] = [];
+        this.drivers.forEach((driver: Driver) => {
+            result.push({
+                driver: driver.name,
+                totalDistance: 0,
+                averageSpeed: 0,
+            });
+        });
+        return result;
+    }
+
+    public addDriver(driver: Driver): void {
+        this.drivers.push(driver);
     }
 }
--- test/summary.test.ts
+++ test/summary.test.ts
@@ -1,10 +1,27 @@
 import { deepStrictEqual } from 'assert';
+import { Driver } from '../src/driver';
 import { Summary } from '../src/summary';
 
 describe(Summary.name, () => {
+    let summary: Summary;
+    beforeEach(() => {
+        summary = new Summary();
+    });
     describe('no data', () => {
         it('#calculate', () => {
-            deepStrictEqual(new Summary().calculate(), []);
+            deepStrictEqual(summary.calculate(), []);
+        });
+    });
+    describe('one driver', () => {
+        it('#calculate', () => {
+            summary.addDriver(new Driver('Lauren'));
+            deepStrictEqual(summary.calculate(), [
+                {
+                    driver: 'Lauren',
+                    totalDistance: 0,
+                    averageSpeed: 0,
+                },
+            ]);
         });
     });
 });
