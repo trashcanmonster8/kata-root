3aa53a6719e45e3f913bb604d5d274b24d5f3ca9
Jake <jacob.michael.mayer@gmail.com>
2021-04-25 17:26:15 -0400

    handles multiple commands

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -8,4 +8,9 @@ describe(Parser.name, () => {
     it('registers a trip', () => {
         strictEqual(new Parser(['Trip Dan 07:15 07:45 17.3']).trips.length, 1);
     });
+    it('takes in a list of commands', () => {
+        const parser: Parser = new Parser(['Driver Dan', 'Trip 1:11 2:01 15']);
+        strictEqual(parser.trips.length, 1);
+        strictEqual(parser.drivers.length, 1);
+    });
 });
