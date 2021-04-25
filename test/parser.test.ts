04441d5615c13862ff52d103ada1c167217bdb08
Jake <jacob.michael.mayer@gmail.com>
2021-04-25 17:15:44 -0400

    renamed tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -2,10 +2,10 @@ import { strictEqual } from 'assert';
 import { Parser } from '../src/parser';
 
 describe(Parser.name, () => {
-    it('registers drivers', () => {
+    it('registers a driver', () => {
         strictEqual(new Parser('Driver Dan').drivers.length, 1);
     });
-    it('registers trips', () => {
+    it('registers a trip', () => {
         strictEqual(new Parser('Trip Dan 07:15 07:45 17.3').trips.length, 1);
     });
 });
